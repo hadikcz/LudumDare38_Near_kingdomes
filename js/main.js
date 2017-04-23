71367162655445c9736b1ee0734bb467b3646787
Hadik <hadikcze@gmail.com>
2017-04-23 19:14:49 +0200

    Fix Gui require for linux server usage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var game;
 
-require(['game', 'World', 'entity/Unit', 'entity/UnitSettings', 'entity/Bullet', 'entity/Tree', 'gui', 'UnitManager', 'HealthBarSprite', 'AI', 'MenuWindow'], function () {
+require(['game', 'World', 'entity/Unit', 'entity/UnitSettings', 'entity/Bullet', 'entity/Tree', 'Gui', 'UnitManager', 'HealthBarSprite', 'AI', 'MenuWindow'], function () {
 	game = new Phaser.Game(1024, 640, Phaser.AUTO, 'game', {preload: preload, create: create, update: update, render: render});
 });
